607412750b724e0042e99654192a44cf10d72a74
Farjana Akter <30821829+Farjana0313@users.noreply.github.com>
2022-05-23 13:33:58 +0600

    Initial commit

---
+++ learnWithSumit/javascript_Function/test.js
@@ -0,0 +1,8 @@
+function sleep(name, time){
+    console.log(name + 'Jasim is sleeping from ' + time);
+    return 5;
+}
+sleep('jashim','10 Pm');
+sleep('rohim','11 Pm');
+sleep('korim','12 Pm');
+console.log("My name Jasim");
